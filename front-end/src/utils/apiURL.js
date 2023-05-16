64ff26e48063f6eb9149e28544d5f51ac958ab80
Henryck <ryck302@gmail.com>
2023-05-16 16:16:09 -0300

    configuração final variáveis ambientes

--- front-end/src/utils/apiURL.js
+++ front-end/src/utils/apiURL.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const HOST = process.env.API_HOST || 'localhost';
-const PROTOCOL = process.env.API_PROTOCOL || 'http';
+const HOST = process.env.REACT_APP_API_HOST || 'localhost';
+const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || 'http';
 
 const api = axios.create({
-  baseURL: `${PROTOCOL}://${HOST}:${process.env.API_HOST ? '' : '3001'}`,
+  baseURL: `${PROTOCOL}://${HOST}:${process.env.REACT_APP_API_HOST ? '' : '3001'}`,
 });
 
 export default api;
